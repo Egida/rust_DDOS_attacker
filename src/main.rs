df2c92cf51fa95708c8600c9c956d6e256a90636
Carghai74 <94583299+carghai@users.noreply.github.com>
2022-09-20 08:22:34 -0400

    more poawar

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ async fn main() {
 
 pub static ATTACK_URL: &str = "https://SecretWindingAttributes.carghai74.repl.co";
 
-pub static FORCE: u128 = 450;
+pub static FORCE: u128 = 1000;
 // 450 to can take down a replit flask api
 // This is the code
 // app.route('/hard')
