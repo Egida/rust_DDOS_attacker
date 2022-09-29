d4db13a61133e18729df4ff514b5baa85677aca7
Carghai74 <94583299+carghai@users.noreply.github.com>
2022-09-29 14:06:45 -0400

    removed dead code

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,6 @@ async fn main() {
     attack::start(init_data).await;
 }
 
-pub static ATTACK_URL: &str = "https://SameSpottedStaff.carghai74.repl.co";
-pub static FORCE: u128 = 6000;
 // 2000 to can take down a replit flask api
 // This is the code
 // app.route('/hard')
