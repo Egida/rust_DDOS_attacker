4675126b92e76536f274893814685d967f97ec1a
Carghai74 <94583299+carghai@users.noreply.github.com>
2022-10-06 13:57:26 -0400

    better error handle

--- src/extra_fn.rs
+++ src/extra_fn.rs
@@ -49,7 +49,7 @@ pub(crate) async fn request() -> Result<Response, Error> {
     }
 }
 
-pub(crate) fn add_start(mut val : MutexGuard<'static,SafeGlobalVar>) {
+pub(crate) fn add_start(mut val: MutexGuard<'static, SafeGlobalVar>) {
     val.thread_on += 1.0;
     unsafe {
         UNSAFE_PUB_VAR.threads_on += 1.0;
@@ -59,15 +59,15 @@ pub(crate) fn add_start(mut val : MutexGuard<'static,SafeGlobalVar>) {
 pub(crate) fn udp() -> UdpSocket {
     let mut error_much: u8 = 0;
     loop {
-        let result  = UdpSocket::bind("0.0.0.0:8080");
-        if let Ok(data) = result {
-           return data;
-        } else {
-            if error_much > 10 {
-                panic!("Failed when starting udp, please check 8080 port and try again");
+        match UdpSocket::bind("0.0.0.0:8080") {
+            Ok(data) => { return data; }
+            Err(_) => {
+                if error_much > 10 {
+                    panic!("Failed when starting udp, please check 8080 port and try again");
+                }
+                thread::sleep(time::Duration::from_millis(20));
+                error_much += 1;
             }
-            thread::sleep(time::Duration::from_millis(20));
-            error_much += 1;
         }
     }
 }
